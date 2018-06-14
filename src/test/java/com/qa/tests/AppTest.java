f5c4c3cdcb5edb35129b5172613591baaf0241e9
Sampath5581 <sampathgoud85@gmail.com>
2018-06-14 17:36:12 +0530

    Updated

---
+++ src/test/java/com/qa/tests/AppTest.java
@@ -0,0 +1,24 @@
+package com.qa.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+/**
+ * Hello world!
+ *
+ */
+public class AppTest
+{
+	@Test
+    public void browser()
+    {
+    	System.setProperty("webdriver.chrome.driver","D:\\chromedriver.exe");
+    	
+    	
+    	WebDriver driver=new ChromeDriver();
+    	driver.get("http://www.assurant.com/");
+    	
+        System.out.println( "Hello World!" );
+    }
+}
